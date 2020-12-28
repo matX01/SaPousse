294e31a21f5705a9ff34bcd03c3fbbd8ffec1277
matX01 <64554454+matX01@users.noreply.github.com>
2020-12-29 00:05:07 +0100

    Update GPSHandling.ino

--- test/GPSHandling/GPSHandling.ino
+++ test/GPSHandling/GPSHandling.ino
@@ -1,84 +1,115 @@
+/*Copyright 2021 , Mathieu CUISSARD
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files of "SaPousse", to deal in the 
+Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, 
+and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE 
+OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+this program is a test to understand how the ubx messages works
+
+    -it contains a retro engeenering tool that allows to record ubx messages send to the gps by the u-center software on the computer
+    -and a test message to pull data from the UBX-CFG-RATES register
+    
+    
+enjoy :)
+
+
+*/
+
 #include "Arduino.h"
 
 char Record[2000] = {0x00};
 bool ReadToken = false;
 int k = 0;
+bool Rd = false;
 
-//char List[9] = {0xB5,0x62,0x06,0x08,0x0E,0x30,0x0A,0x00,0x00};//0x56,0xD2};
-char List[] = {0xB5,0x62,0x06,0x8,0x06,0x03,0x02,0x02,0x00,0x00}; // Checksum
-//{0xB5,0x62,0x06,0x08,0x06,0xE8,0x03,0x02,0x02,0x03,0x3F,0xB5,0x62,0x06,0x08,0x0E,0x30,0x0A};//,0x00,0x00,0x00,0x00,0x00};
+//Token that allows reverse engeneering fonctionalities
 
 #define DEBUG
-//B5 62 6 8 6 E8 3 2 2 3 3F B5 62 6 8 E 30 A 0 0 0 0 0
 
-//B5 62 6 8 6 E8 3 2 2 3 3F B5 62 6 8 E 30 A 0 0 0 0 0 �b�?�b0
+    // ')' character displays the recorded bytes by the reverse engeneering system.
+    // '=' reset the teensy (not the GPS)
+    // '!' send the test message to the secondary port in byte that is written into the "List" array (and displays the list array in hex on the main serial port)
 
 
 
-//B5 62 6 8 E 30 A 0 0 
-//CheckSum : 63 A4
-//B5 62 6 32 38 AE A 
-//B5 62 6 8 E 30
+//char List[] = {0xB5,0x62,0x06,0x08,0x06,0x00,0xE8,0x03,0x02,0x00,0x01,0x00,0x00,0x00}; 
+char List[] = {0xB5,0x62,0x06,0x08,0x00,0x00,0x00,0x00}; 
+
+
+/*Messages sent by U-center while configuring the GPS Rates
+
+    1000ms sampling period
+        B5 62 6 8 6 E8 3 2 2 3 3F B5 62 6 8 E 30 0 0 0 0 0 (There is an error into this line this is not the correct message)
+    Pulling the sampling rate config
+        B5 62 6 8 0 0 E 30 (this is a correct message)
+    */
 
 void setup(){
     
+    //Checksum calculator implementation as shown into the ubx m8 documentation (refer to doc file in the root folder of the repo page 171 chapter 32.4)
+
     uint8_t CK_A = 0;
     uint8_t CK_B = 0;
-    //Serial.println(sizeof(List)-2);
+
     for(int i = 2;i < sizeof(List)-2; i ++){
+        
         CK_A += (uint8_t)List[i];
         CK_B += CK_A;
-        /*CK_A = CK_A & 0xFF;
-        CK_B = CK_B & 0xFF;*/
+
     }
-    //CK_B += ;
+
+    //recording the checksum value at the end of the message
     List[sizeof(List)-1] = (char)CK_B;
     List[sizeof(List)-2] = (char)CK_A;
 
-    Serial.begin(115200);
-    Serial5.begin(115200);
-    
-    delay(1000);
-    
-    //Serial5.print("!UBX CFG-RATE 1000 1 1\r\n");
-    
-
- 
+    //begin the serial ports
+    Serial.begin(115200);                   //Main Serial port (connected to the Computer)
+    Serial5.begin(115200);                  //Second Serial port (connected to the GPS)
 
 }
 
 void loop(){
 #ifdef DEBUG
-while(ReadToken){
-    Serial.print(Record);
-    for(int i = 0;i < k; i ++){
+    //Displaying the recorded bytes sent to the main Serial port in hex
+    while(ReadToken){
+        
+        Serial.print(Record);
+        
+        for(int i = 0;i < k; i ++){
 
-        Serial.print(Record[i],HEX);
-        Serial.print(" ");
+            Serial.print(Record[i],HEX);
+            Serial.print(" ");
+
+        }
+    
+        Serial.println();
+        
+        delay(1000);
 
     }
-    Serial.println();
-    delay(1000);
-
-}
 #endif
 
-
 }
 
-//00000011 11101000
-//11101000 11000000
-//00010111 11000000
-
-
+//if data comes in the main serial port
 void serialEvent(){
-    //Serial.print("Work");
-    char inChar = Serial.read();
- #ifdef DEBUG
-    if(inChar == ')'){
-        ReadToken = true;
-        
 
+    char inChar = Serial.read();
+
+
+ #ifdef DEBUG
+    
+    if(inChar == ')'){
+
+        ReadToken = true;
+    
     }
     else if(inChar == '='){
 
@@ -86,40 +117,58 @@ void serialEvent(){
     
     }
     else if(inChar == '!'){
-
         
+        //send the List message on the secondary serial port and displays the List message in hex on the main serial port
         for(int i = 0;i < sizeof(List); i ++){
+            
             Serial5.write(List[i]);
+            
             Serial.print(List[i],HEX);
             Serial.print(" ");
 
         }
+    
     Serial.println();
    
 
     }
+    
     else if(inChar != 0x0A){
+        
+        //records the input data
         k ++;
+        
         sprintf(Record,"%s%c",Record,inChar);
-#endif
+
+        //send the input to the gps as a simple bridge beetween the main and the secondary port
         Serial5.write(inChar);
-#ifdef DEBUG
+    
     }
+      
 #endif
- 
+#ifndef DEBUG
+
+    //send the input as a simple bridge beetween the main and the secondary port
+    Serial5.write(inChar);
+
+#endif
 
 }
 
-bool Rd = false;
+//if data comes in the secondary serial port
 void serialEvent5(){
-    //Serial.print("Work");
+
     char InByte = Serial5.read();
-    #ifdef DEBUG
+
+#ifdef DEBUG
+    //if its an NMEA message , displays it as it comes
+    
     if(InByte == '$'){
 
         Rd = true;
 
     }
+
     if(!ReadToken && Rd){
         
         Serial.write(InByte);
@@ -127,24 +176,31 @@ void serialEvent5(){
     }
 
     if(!Rd){
-
+        //and if it's not (so it's a UBX message) then displays it in hex on the main serial port
         Serial.print(InByte,HEX);
         Serial.print(" ");
 
 
 
     }
+
     if(InByte == '\n'){
 
         Rd = false;
 
     }
-    #endif
-    #ifndef DEBUG
-     Serial.write(InByte);
-     #endif
+
+#endif
+#ifndef DEBUG
+     //send the input as a simple bridge beetween the secondary and the main port
+    Serial.write(InByte);
+
+#endif
 
 }
+
+
+//U-center copy of packet console when a ubx config is sent to the gps
 /*21:59:56  R -> NMEA GPGSV,  Size  64,  'GNSS Satellites in View'
 21:59:56  R -> NMEA GPGSV,  Size  70,  'GNSS Satellites in View'
 21:59:56  R -> NMEA GPGSV,  Size  57,  'GNSS Satellites in View'
@@ -166,20 +222,15 @@ void serialEvent5(){
 21:59:57  R -> NMEA GLGSV,  Size  66,  'GNSS Satellites in View'
 21:59:57  R -> NMEA GLGSV,  Size  31,  'GNSS Satellites in View'
 21:59:57  R -> NMEA GNGLL,  Size  52,  'Geographic Position - Latitude/Longitude'
-21:59:57  R -> UBX ACK-ACK,  Size  10,  'Acknowledged'
-21:59:57  R -> UBX CFG-RATE,  Size  14,  'Rates'
-21:59:57  R -> UBX ACK-ACK,  Size  10,  'Acknowledged'
+
+
+                        21:59:57  R -> UBX ACK-ACK,  Size  10,  'Acknowledged'
+                        21:59:57  R -> UBX CFG-RATE,  Size  14,  'Rates'
+                        21:59:57  R -> UBX ACK-ACK,  Size  10,  'Acknowledged'
+
+
 21:59:57  R -> NMEA GNRMC,  Size  68,  'Recommended Minimum Specific GNSS Data'
 21:59:57  R -> NMEA GNVTG,  Size  35,  'Course Over Ground and Ground Speed'
 21:59:57  R -> NMEA GNGGA,  Size  74,  'Global Positioning System Fix Data'
 21:59:57  R -> NMEA GNGSA,  Size  52,  'GNSS DOP and Active Satellites'
 
-
-          0030  37 37 0D 0A                                      77
-          
-22:04:21  0000  B5 62 06 08 06 00 10 27 02 00 02 00 4F E3        µb'Oã
-          
-22:04:21  0000  B5 62 05 01 02 00 06 08 16 3F    
-
-B5 62 6 8 6 E8 3 2 2 3 3F B5 62 6 8 E 30 A 0 0 0 0 0
-*/
